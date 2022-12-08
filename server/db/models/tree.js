46f6e4693ef5694fdffe2fb2b4b7ea5616c3421a
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-12-07 16:19:40 -0800

    phase5 done

--- server/db/models/tree.js
+++ server/db/models/tree.js
@@ -11,6 +11,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Tree.belongsToMany(models.Insect,
+        {
+          through: models.InsectTrees,
+          foreignKey: "treeId",
+          otherKey: "insectId"
+        }
+      )
     }
   };
   Tree.init({
